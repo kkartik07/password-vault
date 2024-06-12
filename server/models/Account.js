5297dd5ae65602ac1074930f4fc699fd053f5d08
Kartik Kankurte <kartikkankurte@gmail.com>
2024-06-12 13:13:18 +0530

    made account schema and model

---
+++ server/models/Account.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+const accountSchema=new mongoose.Schema({
+    accountName: {
+        type: String,
+        required: true
+    },
+    accountPassword: {
+        type: String,
+        required: true
+    },
+},{timestamps:true});
+
+const Account=mongoose.model('account',accountSchema);
+
+module.exports={Account,accountSchema}; 
