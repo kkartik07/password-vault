db0517b25bbcf4c154975b7c85cf4ba029131554
Kartik Kankurte <kartikkankurte@gmail.com>
2024-06-14 01:03:04 +0530

    worked on initial fetch of accounts

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import CreateModal from "./CreateModal";
 import { Button } from "@mui/material";
 import AccountCard from "./AccountCard";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
+
 
 function Home() {
-    
     const [canCreate, setCanCreate] = useState(false);
     const [accounts, setAccounts] = useState([
-        { name: "Facebook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Faceb1ook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Face2book", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Facebo5ok", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Fa35cebook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Faceboo6k", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Faceb3ook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Faceb8ook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Fa32cebook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Fac4ook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Fac444ebook", password: "kartik123", email: 'kartik@gmail.com' },
-        { name: "Gmail", password: "kfdfdf3", email: 'kartdddik@gmail.com' },
-        // Add other accounts here...
+        { accountName: "Facebook", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Faceb1ook", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Face2book", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Facebo5ok", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Fac4ook", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Fac444ebook", accountPassword: "kartik123", accountEmail: 'kartik@gmail.com' },
+        { accountName: "Gmail", accountPassword: "kfdfdf3", accountEmail: 'kartdddik@gmail.com' },
+        // use shimmer ui cards instead in future !!!!!!!!!!!!!!
     ]);
 
     const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        async function getAccounts(){
+            try {
+                const token=localStorage.getItem('token');
+                const userID=localStorage.getItem('user-id');
+                const res = await axios.get(`http://localhost:3001/${userID}/accounts`,{headers:{token,userID}});
+                setAccounts(res.data);
+            } catch (err) {
+                console.error(err); // Handle errors
+                const notify = () => toast("Session expired. Login Again");
+                notify();
+                setTimeout(()=>navigate('/login'),5000)
+            }
+        }
+        getAccounts();
+    },[])
 
     const handleSearch = (query) => {
         console.log(query)
@@ -33,10 +49,8 @@ function Home() {
         setCanCreate(canCreate => !canCreate);
     };
     
-    
-
     const filteredAccounts = accounts.filter(account =>
-        account.name.toLowerCase().includes(searchQuery.toLowerCase())
+        account.accountName.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
     return (
@@ -51,10 +65,18 @@ function Home() {
             </div>
             <div className="cards">
                 {filteredAccounts.map(account => (
-                    <AccountCard account={account} key={account.name}/>
+                    <AccountCard account={account} key={account.accountName}/>
                 ))}
             </div>
-            {canCreate && <CreateModal handleCreateToggler={handleCreateToggler} />}
+            {canCreate && <CreateModal handleCreateToggler={handleCreateToggler} setAccounts={setAccounts}/>}
+            <Toaster 
+               toastOptions={{
+                    style: {
+                    padding: '12px',
+                    color: '#713200',
+                    background: '#fccccc'
+                    },
+              }}/>
         </div>
     );
 }
